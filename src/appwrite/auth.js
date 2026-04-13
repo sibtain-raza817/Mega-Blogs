0e96a9755b5c75b88a8d3656a234f03e3047120b
Sibtain Raza <khanbhai14063@gmail.com>
2026-04-13 23:46:04 +0530

    Complete auth file start to end ...

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,5 +1,5 @@
 import config from '../config.js'
-import { Client, Account } from 'appwrite'
+import { Client, Account, ID } from 'appwrite'
 
 
 export class AuthService{
@@ -11,6 +11,47 @@ export class AuthService{
         .setEndpoint(config.appwriteUrl)
         .setProject(config.appwriteProjectId);
         this.account = new Account(this.client)
+    }
+
+    async createAccount({email, password, name}){
+        try {
+            const userAccount = await this.account.create
+            (ID.unique(), email, password, name);
+            if(userAccount){
+                return userAccount;
+                //call another methods
+                return this.login({email, password});
+            }else{
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+           return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+            await this.account.deleteSessions('current')
+        } catch (error) {
+            console.log("Appwrite service :: logout :: errro", error);
+            
+        }
     }
 }
 
