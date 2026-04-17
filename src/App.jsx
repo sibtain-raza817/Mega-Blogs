96e09a3ffdaa6af8cf7d3cb126205043d82cac23
Sibtain Raza <khanbhai14063@gmail.com>
2026-04-17 22:37:23 +0530

    Store file has been complete today

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
 
+import { useState, useEffect } from 'react';
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from "./appwrite/auth"
 
 function App() {
   console.log(import.meta.env.VITE_APPWRITE_URL);
   
-
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch()
   return (
     <>
       <h1>A blog with appwrite</h1>
